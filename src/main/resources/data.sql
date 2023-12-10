19687cd45b2a3133afd50098f3272513e9861f13
luvlaceeeee <nikitan2002@gmail.com>
2023-12-11 01:22:45 +0300

    feat: added producers

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into producer_entity (id, name, address) values (1 ,'Ivan', 'g.Kasimov');
-insert into producer_entity (id, name, address) values (2, 'Mikhail', 'g.Michurinsk');
-insert into producer_entity (id, name, address) values (3, 'Oleg', 'g.Tinkoffland');
-insert into producer_entity (id, name, address) values (4, 'Vlad', 'g.Gantelya');
-insert into producer_entity (id, name, address) values (5, 'Konor', 'g.McGregor');
+insert into producer_entity (id, name, address) values (1 ,'BRAER', 'g.Kasimov');
+insert into producer_entity (id, name, address) values (2, 'MSTERA', 'g.Michurinsk');
+insert into producer_entity (id, name, address) values (3, 'EUROBLOCK', 'g.Tinkoffland');
+insert into producer_entity (id, name, address) values (4, 'LEONARDO STONE', 'g.Gantelya');
+insert into producer_entity (id, name, address) values (5, 'PERFEKTA', 'g.McGregor');
